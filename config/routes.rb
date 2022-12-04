df459cc9dc1f3561bbf4561aba214c5d346c4be3
yongin6412 <rladls6412@naver.com>
2022-12-04 05:26:09 +0000

    test commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 	get 'mainpage', to:'page#mainpage'
-	get 'page' ,to:'page#page1'
-	get 'first', to:'page#firstpage'
-		
 	
+	
+	get 'first', to:'page#firstpage'
 	get 'menupage', to:'page#menupage'
 	get 'chicken', to:'page#chicken'
 	get 'chickenmenu', to:'page#chickenmenu'
 	get 'shopping', to:'page#shopping'
 	get 'mypage', to:'page#mypage'
-	get 'reviewpage', to:'page#reviewpage'
 	get 'login', to:'page#loginpage'
 	get 'order1', to:'page#orderpage1'
 	get 'ceo' , to:'page#ceopage'
 	get 'signup' , to:'page#signuppage'
-	
+	get 'storeadd' , to:'page#storeadd'
 	
 	# root 'page#menupage'
 	
